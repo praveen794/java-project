d2c04a5bb5c54b4a986c538fdd93b87cbb9dc84d
Prawin <praveenkumar16661@gmail.com>
2022-04-16 10:39:29 +0530

    this my first commit

---
+++ Praveeshasank/src/com/pravee/javaLearning/Multiplication.java
@@ -0,0 +1,25 @@
+package com.pravee.javaLearning;
+
+public class Multiplication {
+	
+public static void main(String[] args)
+{
+ 
+int a=500,b=400,c=600,d,e,f,g,h,i;
+    d=a*b*c;
+    e=d/1000;
+              f=c*100;
+              g=b*100;
+              h=a*100;
+              i=e*100;
+    {
+    System.out.println(d);
+    System.out.println(e);
+    System.out.println(f);
+    System.out.println(g);
+    System.out.println(h);
+    System.out.println(i);
+   }
+ }
+}
+
---
+++ Praveeshasank/src/com/pravee/javaLearning/SimpleInterst.java
@@ -0,0 +1,12 @@
+package com.pravee.javaLearning;
+
+public class SimpleInterst 
+{
+	public static void main (String[] args) {
+	int P=100000,T=12,R=2,Si;
+	Si=P*T*R/100;
+
+	System.out.println(Si);
+	
+}
+}
